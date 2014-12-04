f6d8a09d31ec8779e28d30cd5cecd50cc7b954c1
jlrogers1 <jlrogers1@lcmail.lcsc.edu>
2014-12-04 15:51:21 -0800

    Tabs
    Added tabs, removed load button, added a bunch of try/catch's.

--- DBProject/Form1.Designer.vb
+++ DBProject/Form1.Designer.vb
@@ -22,51 +22,167 @@ Partial Class EmployeeEdit
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.EmployeeDataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.WorksInDataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.EmployeeSkillsDataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.SkillDataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.EmployeeDataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.WorksInDataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.EmployeeSkillsDataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage4.SuspendLayout()
+        CType(Me.SkillDataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'EmployeeDataGridView1
         '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
-        Me.Button1.Location = New System.Drawing.Point(0, 282)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(602, 45)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Get All Employees"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.EmployeeDataGridView1.AllowUserToAddRows = False
+        Me.EmployeeDataGridView1.AllowUserToDeleteRows = False
+        Me.EmployeeDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.EmployeeDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.EmployeeDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.EmployeeDataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.EmployeeDataGridView1.Name = "EmployeeDataGridView1"
+        Me.EmployeeDataGridView1.ReadOnly = True
+        Me.EmployeeDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.EmployeeDataGridView1.Size = New System.Drawing.Size(588, 295)
+        Me.EmployeeDataGridView1.TabIndex = 1
         '
-        'DataGridView1
+        'TabControl1
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(602, 276)
-        Me.DataGridView1.TabIndex = 1
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(602, 327)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.EmployeeDataGridView1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(594, 301)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Employees"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.WorksInDataGridView1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(594, 301)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Work Location"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'WorksInDataGridView1
+        '
+        Me.WorksInDataGridView1.AllowUserToAddRows = False
+        Me.WorksInDataGridView1.AllowUserToDeleteRows = False
+        Me.WorksInDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.WorksInDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.WorksInDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WorksInDataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.WorksInDataGridView1.Name = "WorksInDataGridView1"
+        Me.WorksInDataGridView1.ReadOnly = True
+        Me.WorksInDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.WorksInDataGridView1.Size = New System.Drawing.Size(588, 295)
+        Me.WorksInDataGridView1.TabIndex = 2
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.EmployeeSkillsDataGridView1)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(594, 301)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Emp Skills"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'EmployeeSkillsDataGridView1
+        '
+        Me.EmployeeSkillsDataGridView1.AllowUserToAddRows = False
+        Me.EmployeeSkillsDataGridView1.AllowUserToDeleteRows = False
+        Me.EmployeeSkillsDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.EmployeeSkillsDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.EmployeeSkillsDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.EmployeeSkillsDataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.EmployeeSkillsDataGridView1.Name = "EmployeeSkillsDataGridView1"
+        Me.EmployeeSkillsDataGridView1.ReadOnly = True
+        Me.EmployeeSkillsDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.EmployeeSkillsDataGridView1.Size = New System.Drawing.Size(588, 295)
+        Me.EmployeeSkillsDataGridView1.TabIndex = 3
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Controls.Add(Me.SkillDataGridView1)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(594, 301)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Skills"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'SkillDataGridView1
+        '
+        Me.SkillDataGridView1.AllowUserToAddRows = False
+        Me.SkillDataGridView1.AllowUserToDeleteRows = False
+        Me.SkillDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.SkillDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.SkillDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SkillDataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.SkillDataGridView1.Name = "SkillDataGridView1"
+        Me.SkillDataGridView1.ReadOnly = True
+        Me.SkillDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.SkillDataGridView1.Size = New System.Drawing.Size(588, 295)
+        Me.SkillDataGridView1.TabIndex = 3
         '
         'EmployeeEdit
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(602, 327)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "EmployeeEdit"
-        Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "PVF Employee Management System"
+        CType(Me.EmployeeDataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.WorksInDataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
+        CType(Me.EmployeeSkillsDataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage4.ResumeLayout(False)
+        CType(Me.SkillDataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents EmployeeDataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents WorksInDataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents EmployeeSkillsDataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents SkillDataGridView1 As System.Windows.Forms.DataGridView
 
 End Class
--- DBProject/Form1.vb
+++ DBProject/Form1.vb
@@ -1,26 +1,62 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class EmployeeEdit
     Dim MySqlConn As MySqlConnection
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         MySqlConn = New MySqlConnection
         MySqlConn.ConnectionString = "server=isoptera.lcsc.edu;port=3306;userid=CS;password=cs445;database=cs445"
         Try
             MySqlConn.Open()
-            MessageBox.Show("Connecton Established!")
-            Dim MyTable As New DataTable()
             Dim MySqlCmd As New MySqlCommand()
             MySqlCmd.Connection = MySqlConn
-            MySqlCmd.CommandText = "SELECT * FROM Employee_T"
-            Dim MyAdapter As New MySqlDataAdapter(MySqlCmd)
-            MyAdapter.Fill(MyTable)
-            DataGridView1.DataSource = MyTable
-            MySqlConn.Close()
-            MyTable.Dispose()
+            Try 'SELECT ALL EMPLOYEES
+                MySqlCmd.CommandText = "SELECT * FROM Employee_T"
+                Dim EmployeeAdapter As New MySqlDataAdapter(MySqlCmd)
+                Dim EmployeeTable As New DataTable()
+                EmployeeAdapter.Fill(EmployeeTable)
+                EmployeeDataGridView1.DataSource = EmployeeTable
+                EmployeeTable.Dispose()
+                EmployeeAdapter.Dispose()
+            Catch
+                MessageBox.Show("An error occured when loading Employee_T")
+            End Try
+            Try 'SELECT ALL LOCATIONS EMPLOYEES WORK
+                MySqlCmd.CommandText = "SELECT * FROM WorksIn_T"
+                Dim WorksInAdapter As New MySqlDataAdapter(MySqlCmd)
+                Dim WorksInTable As New DataTable()
+                WorksInAdapter.Fill(WorksInTable)
+                WorksInDataGridView1.DataSource = WorksInTable
+                WorksInTable.Dispose()
+                WorksInAdapter.Dispose()
+            Catch
+                MessageBox.Show("An error occured when loading WorksIn_T")
+            End Try
+            Try 'SELECT ALL SKILLS EMPLOYEES HAVE
+                MySqlCmd.CommandText = "SELECT * FROM EmployeeSkills_T"
+                Dim EmployeeSkillsAdapter As New MySqlDataAdapter(MySqlCmd)
+                Dim EmployeeSkillsTable As New DataTable()
+                EmployeeSkillsAdapter.Fill(EmployeeSkillsTable)
+                EmployeeSkillsDataGridView1.DataSource = EmployeeSkillsTable
+                EmployeeSkillsTable.Dispose()
+                EmployeeSkillsAdapter.Dispose()
+            Catch
+                MessageBox.Show("An error occured when loading EmployeeSkills_T")
+            End Try
+            Try 'SELECT ALL SKILLS
+                MySqlCmd.CommandText = "SELECT * FROM Skill_T"
+                Dim SkillAdapter As New MySqlDataAdapter(MySqlCmd)
+                Dim SkillTable As New DataTable()
+                SkillAdapter.Fill(SkillTable)
+                SkillDataGridView1.DataSource = SkillTable
+                SkillTable.Dispose()
+                SkillAdapter.Dispose()
+            Catch
+                MessageBox.Show("An error occured when loading Skill_T")
+            End Try
             MySqlCmd.Dispose()
-            MyAdapter.Dispose()
         Catch ex As MySqlException
             MessageBox.Show(ex.Message)
         Finally
+            MySqlConn.Close()
             MySqlConn.Dispose()
         End Try
     End Sub
