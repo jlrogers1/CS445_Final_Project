6e4ac8d43e295da8126ca8c9e880d91979a203a8
mcsteele <mcsteele@lcmail.lcsc.edu>
2014-12-09 10:15:38 -0800

    finished login graphics

--- DBProject/auth.vb
+++ DBProject/auth.vb
@@ -2,15 +2,16 @@
 
 Public Class auth
     Dim MySqlConn As MySqlConnection
-
+    Dim COMMAND As MySqlCommand
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MySqlConn = New MySqlConnection
-        MySqlConn.ConnectionString = "server=isoptera.lcsc.edu;port=3306;userid=CS;password=cs445;database=cs445"
-
+        MySqlConn.ConnectionString = "server=isoptera.lcsc.edu;port=3306;userid='" & TextBox_UN.Text & "';password='" & TextBox_Pwd.Text & "';database='" & TextBox_DB.Text & "'"
         Try
             MySqlConn.Open()
-            Dim Command As MySqlCommand
+            MessageBox.Show("Connection Established!")
+            
+
 
             MySqlConn.Close()
             
@@ -19,5 +20,21 @@ Public Class auth
         Finally
             MySqlConn.Dispose()
         End Try
+    End Sub
+
+    Private Sub TextBox_UN_TextChanged(sender As Object, e As EventArgs) Handles TextBox_UN.TextChanged
+
+    End Sub
+
+    Private Sub TextBox_Pwd_TextChanged(sender As Object, e As EventArgs) Handles TextBox_Pwd.TextChanged
+
+    End Sub
+
+    Private Sub TextBox_DB_TextChanged(sender As Object, e As EventArgs) Handles TextBox_DB.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
     End Sub
 End Class
