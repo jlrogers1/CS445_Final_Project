555c0b40df45ed17c872cf4c3072221d10d947f3
jlrogers1 <jlrogers1@lcmail.lcsc.edu>
2014-12-11 02:00:56 -0800

    DONE!
    It appears to be working as well as can be expected...

---
+++ DBProject/auth.designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class auth
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox_UN = New System.Windows.Forms.TextBox()
+        Me.TextBox_Pwd = New System.Windows.Forms.TextBox()
+        Me.TextBox_DB = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Button1.BackgroundImage = Global.DBProject.My.Resources.Resources.cooltext1836774822
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 3
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(231, 65)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox_UN
+        '
+        Me.TextBox_UN.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox_UN.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_UN.Location = New System.Drawing.Point(12, 126)
+        Me.TextBox_UN.Name = "TextBox_UN"
+        Me.TextBox_UN.Size = New System.Drawing.Size(396, 26)
+        Me.TextBox_UN.TabIndex = 2
+        '
+        'TextBox_Pwd
+        '
+        Me.TextBox_Pwd.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox_Pwd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_Pwd.Location = New System.Drawing.Point(12, 182)
+        Me.TextBox_Pwd.Name = "TextBox_Pwd"
+        Me.TextBox_Pwd.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox_Pwd.Size = New System.Drawing.Size(396, 26)
+        Me.TextBox_Pwd.TabIndex = 3
+        '
+        'TextBox_DB
+        '
+        Me.TextBox_DB.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox_DB.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_DB.Location = New System.Drawing.Point(12, 245)
+        Me.TextBox_DB.Name = "TextBox_DB"
+        Me.TextBox_DB.Size = New System.Drawing.Size(211, 26)
+        Me.TextBox_DB.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(12, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Location = New System.Drawing.Point(12, 166)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label3.Location = New System.Drawing.Point(12, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Database"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.DBProject.My.Resources.Resources.cooltext1836750849
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(621, 88)
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'auth
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.BackgroundImage = Global.DBProject.My.Resources.Resources.harbor_ship_sea_mast_water_pirate_boat_sailboat
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1214, 630)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox_DB)
+        Me.Controls.Add(Me.TextBox_Pwd)
+        Me.Controls.Add(Me.TextBox_UN)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "auth"
+        Me.Text = "Authentication"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox_UN As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox_Pwd As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox_DB As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ DBProject/auth.vb
@@ -0,0 +1,36 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class auth
+    Dim MySqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Dim Authenticated = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox_DB.Text = "cs445"
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=isoptera.lcsc.edu;port=3306;userid='" + TextBox_UN.Text + "';password='" + TextBox_Pwd.Text + "';database='" + TextBox_DB.Text + "'"
+        Try
+            MySqlConn.Open()
+            MySqlConn.Close()
+            Authenticated = True
+        Catch exError As MySqlException
+            Authenticated = False
+            MsgBox("An Error Occurred. " & exError.Number & " – " & exError.Message)
+        Finally
+            MySqlConn.Dispose()
+            Try
+                If Authenticated = True Then
+                    Try
+                        Dim PVFMain As New EmployeeEdit()
+                        Me.Hide()
+                        PVFMain.Show()
+                    Catch
+                        MsgBox("An Error Occurred.")
+                    End Try
+                End If
+            Catch
+                Authenticated = False
+                MsgBox("An Error Occurred.")
+            End Try
+        End Try
+    End Sub
+End Class
